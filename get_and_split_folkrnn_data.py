ce4fbc9caae8ed8219d776291ae81064e1b2eaf8
JamesOwers <james.f.owers+github@gmail.com>
2020-09-22 13:57:56 +0100

    Create vocab too.

--- get_and_split_folkrnn_data.py
+++ get_and_split_folkrnn_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Script to download and create the splits for folkrnn data."""
+"""Script to download folkrnn data, extract the vocab, and define splits."""
 import fire
 import logging
 from pathlib import Path
@@ -31,17 +31,21 @@ def round_to_nearest_batch_size(
     return rounded_count
 
 
-def make_splits(
+def download_get_tokens_and_make_splits(
     data_path: str = './data_v3',
     url: str = FOLKRNN_V3_URL,
     seed: int = 42,
-    percentile: float = .99,
+    length_percentile: float = .99,
     test_prop: float = .05,
     valid_prop: float = .05,
     train_prop: float = .9,
     batch_size: int = 64,
 ) -> None:
     """"""
+    assert test_prop + valid_prop + train_prop == 1, (
+        "train, valid, and test prop must sum to 1"
+    )
+    
     data_path = Path(data_path).resolve()
     data_path.parent.mkdir(parents=True, exist_ok=True)
     download_file(url, data_path)
@@ -50,18 +54,28 @@ def make_splits(
         data = fh.read()
 
     tunes = [tune.split() for tune in data.split('\n\n')]
+    # Save vocabulary prior to subsetting
+    LOGGER.info("Extracting vocabulary")
+    all_tokens = [token for tune in tunes for token in tune]
+    all_tokens = sorted(list(set(all_tokens)))
+    LOGGER.info(f"vocabulary size: {len(all_tokens)}")
+    LOGGER.info(
+        f"vocabulary (each token separated by a space): \n{' '.join(all_tokens)}"
+    )
+    filepath = str(data_path) + f"_vocabulary.txt" 
+    with open(filepath, "w") as file_handle:
+        file_handle.write("\n".join(all_tokens))
+    
     tune_lens = np.array([len(t) for t in tunes])
 
-    nr_kept = int(np.rint(len(tune_lens)*percentile))
+    nr_kept = int(np.rint(len(tune_lens)*length_percentile))
     max_tune_len = sorted(tune_lens)[nr_kept - 1]
-    LOGGER.info(f"Choosing the shortest {percentile*100}% of tunes for train/valid/test")
+    LOGGER.info(
+        f"Choosing the shortest {length_percentile*100}% of tunes for train/valid/test"
+    )
     LOGGER.info(f"They are all {max_tune_len} tokens long or less")
     tune_indexes = np.array(
         [idx for idx, tune in enumerate(tunes) if len(tune) <= max_tune_len]
-    )
-    
-    assert test_prop + valid_prop + train_prop == 1, (
-        "train, valid, and test prop must sum to 1"
     )
     
     nr_tunes = len(tune_indexes)
@@ -97,8 +111,8 @@ def make_splits(
         filepath = str(data_path) + f"_{split_name}_split.txt" 
         with open(filepath, "w") as file_handle:
             file_handle.write("\n".join([str(ii) for ii in idx]))
-            
+
             
 if __name__ == "__main__":
-    fire.Fire(make_splits)
+    fire.Fire(download_get_tokens_and_make_splits)
     
