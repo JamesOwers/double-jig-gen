26875753c8f8a7d553d446ee4487eaa7bd221d39
JamesOwers <james.f.owers+github@gmail.com>
2021-04-05 10:33:37 +0100

    Add function to tokenize abc files

--- double_jig_gen/tokenizers.py
+++ double_jig_gen/tokenizers.py
@@ -1,6 +1,9 @@
 """Classes which turn strings into lists of tokens."""
 import logging
-from typing import Collection, List, Optional, Sequence, Union
+import re
+from typing import Collection, List, Mapping, Optional, Sequence, Union
+
+import music21
 
 LOGGER = logging.getLogger(__name__)
 
@@ -37,7 +40,9 @@ class Tokenizer:
     # TODO: Make __call__ which (un)tokenizes automagically
 
     def tokenize(
-        self, str_or_sequence: Union[str, Sequence[str]], wrap: bool = True,
+        self,
+        str_or_sequence: Union[str, Sequence[str]],
+        wrap: bool = True,
     ) -> List[int]:
         if isinstance(str_or_sequence, str):
             token_sequence = str_or_sequence.split()
@@ -62,3 +67,184 @@ class Tokenizer:
             int_sequence = list(str_or_sequence)
         token_sequence = [self.tokens[index] for index in int_sequence]
         return token_sequence
+
+
+# Taken from: http://abcnotation.com/wiki/abc:standard:v2.1
+ABC_FIELDS = {
+    "A": "area",
+    "B": "book",
+    "C": "composer",
+    "D": "discography",
+    "F": "file url",
+    "G": "group",
+    "H": "history",
+    "I": "instruction",
+    "K": "key",
+    "L": "unit note length",
+    "M": "meter",
+    "m": "macro",
+    "N": "notes",
+    "O": "origin",
+    "P": "parts",
+    "Q": "tempo",
+    "R": "rhythm",
+    "r": "remark",
+    "S": "source",
+    "s": "symbol line",
+    "T": "tune title",
+    "U": "user defined",
+    "V": "voice",
+    "W": "words",
+    "w": "words",
+    "X": "reference number",
+    "Z": "transcription",
+}
+
+
+def merge_continuation_lines(lines: Sequence[str]) -> Sequence[str]:
+    r"""Merges lines which end with \ with the next line.
+
+    Args:
+        lines: a list of strings to check and merge.
+
+    Returns:
+        lines: the list of strings which have been merged.
+    """
+    nr_checks = len(lines)
+    idx = 0
+    for _ in range(nr_checks):
+        line = lines[idx]
+        if line.endswith("\\"):
+            line = line[:-1]  # remove trailing backslash
+            try:
+                lines[idx] = f"{line} {lines[idx+1]}"
+                del lines[idx + 1]  # del and stay on this line
+            except IndexError as e:
+                # don't care if last line - just remove slash
+                if (idx + 1) == nr_checks:
+                    lines[idx] = f"{line}"
+                else:
+                    print(lines, line)
+                    raise e
+        else:
+            idx += 1
+    return lines
+
+
+def abc_to_events(abc_str):
+    abc = music21.converter.parse(abc_str, format="abc")
+    # N.B. calling .flat on abc makes offset times stored in not.offset relative to the start of the piece, rather than relative to the containing stream.
+    note_stream = abc.flat.getElementsByClass(["Note", "Chord"])
+    events = []
+    for element in note_stream:
+        if isinstance(element, music21.note.Note):
+            events.append(
+                dict(
+                    start_quartertime=element.offset,
+                    midipitch=element.pitch.ps,
+                    pitch_str=f"{element.name.replace('-', 'b')}{element.octave}",
+                    accidental=element.pitch.accidental,
+                    pitch_class=element.pitch.pitchClass,
+                    beat_dur=element.duration.quarterLength,
+                )
+            )
+        if isinstance(element, music21.chord.Chord):
+            for pitch in element.pitches:
+                events.append(
+                    dict(
+                        start_quartertime=element.offset,
+                        midipitch=pitch.ps,
+                        pitch_str=f"{pitch.name.replace('-', 'b')}{pitch.octave}",
+                        accidental=pitch.accidental,
+                        pitch_class=pitch.pitchClass,
+                        beat_dur=element.duration.quarterLength,
+                    )
+                )
+    return events
+
+
+def parse_abc(abc_str: str) -> Mapping[str, str]:
+    """Get the required information from each tune.
+
+    Extracts the metadata as defined in [1] from the string, plus the transcription
+    which is assumed not to have a prefix. Assumes all metadata is stated before the
+    transcription is started. Then adds everything remaining to the transcription.
+
+    Args:
+        abc_str: the string containing an abc file to parse.
+
+    Returns:
+        out_dict: a dictionary containing all the extracted values.
+
+    See also:
+    [1] http://abcnotation.com/wiki/abc:standard:v2.1
+    """
+    lines = [line.strip() for line in abc_str.split("\n")]
+
+    # if a line ends with \ then merge with subsequent line
+    lines = merge_continuation_lines(lines)
+
+    valid_field = "|".join(ABC_FIELDS)
+    metadata_line_regex = re.compile(f"^({valid_field}):")
+
+    _, metadata_end_idx = min(
+        (val, idx)
+        for (idx, val) in enumerate(
+            [bool(metadata_line_regex.match(line)) for line in lines]
+        )
+    )
+
+    out_dict = {}
+    for line in lines[:metadata_end_idx]:
+        meta_key, value = line.split(":", 1)
+        field_name = ABC_FIELDS[meta_key]
+        if field_name in out_dict:
+            concat_fields = ("tune title", "words")
+            if field_name not in concat_fields:
+                msg = (
+                    f"Tried to add {(field_name, value)} to {out_dict}."
+                    f"\nLines: {lines}"
+                )
+                raise ValueError(msg)
+            else:
+                out_dict[field_name] = f"{out_dict[field_name]} --- {line}"
+        out_dict[field_name] = value
+    if "meter" in out_dict:
+        tune_lines = [f"M:{out_dict['meter']}"]
+    else:
+        raise ValueError("An abc tune must have a meter")
+    if "key" in out_dict:
+        tune_lines += [f"K:{out_dict['key']}"]
+    else:
+        raise ValueError("An abc tune must have a key")
+    for line in lines[metadata_end_idx:]:
+        # If the line starts with a W: we assume the whole thing is words and move on
+        if line.upper().startswith("W:"):
+            if "words" not in out_dict:
+                out_dict["words"] = line
+            else:
+                out_dict["words"] = f"{out_dict['words']} --- {line}"
+            continue
+        # TODO: may require manual handling of things here e.g. mid line key changes
+        tune_lines.append(line)
+    out_dict["tune_lines"] = tune_lines
+    out_dict["tune_str"] = "\n".join(out_dict["tune_lines"])
+    return out_dict
+
+
+class ABCParser:
+    """Takes a string containing a single tune, and codifies the information."""
+
+    def __init__(self, abc_str):
+        self.abc_str = abc_str
+        self.abc_dict = parse_abc(abc_str)
+        self.tune_str = self.abc_dict["tune_str"]
+        self.abc_handler = music21.abcFormat.ABCHandler()
+        self._tokens = None
+
+    @property
+    def tokens(self):
+        if self._tokens is None:
+            self.abc_handler.tokenize(self.tune_str)
+            self._tokens = [token.src for token in self.abc_handler.tokens]
+        return self._tokens
--- tests/test_tokenizers.py
+++ tests/test_tokenizers.py
@@ -1,6 +1,7 @@
+import music21
 import pytest
 
-from double_jig_gen.tokenizers import Tokenizer
+from double_jig_gen.tokenizers import Tokenizer, abc_to_events
 
 
 def test_tokenizer():
@@ -24,3 +25,108 @@ def test_tokenizer_special_tokens():
     for token, expected_exception in test_data.items():
         with pytest.raises(expected_exception):
             _ = Tokenizer(tokens=["hi", token])
+
+
+@pytest.fixture()
+def music21_test_examples():
+    examples = []
+    examples.append(
+        {
+            "in": "L:1/16\nK:C#maj\n[CG_e]",
+            "out": [
+                {
+                    "start_quartertime": 0.0,
+                    "midipitch": 61.0,
+                    "pitch_str": "C#4",
+                    "accidental": music21.pitch.Accidental("#"),
+                    "pitch_class": 1,
+                    "beat_dur": 0.25,
+                },
+                {
+                    "start_quartertime": 0.0,
+                    "midipitch": 68.0,
+                    "pitch_str": "G#4",
+                    "accidental": music21.pitch.Accidental("#"),
+                    "pitch_class": 8,
+                    "beat_dur": 0.25,
+                },
+                {
+                    "start_quartertime": 0.0,
+                    "midipitch": 75.0,
+                    "pitch_str": "Eb5",
+                    "accidental": music21.pitch.Accidental("-"),
+                    "pitch_class": 3,
+                    "beat_dur": 0.25,
+                },
+            ],
+        }
+    )
+    examples.append(
+        {
+            "in": "L:1/8\nK:C\nCG_e[CG_e]",
+            "out": [
+                {
+                    "start_quartertime": 0.0,
+                    "midipitch": 60.0,
+                    "pitch_str": "C4",
+                    "accidental": None,
+                    "pitch_class": 0,
+                    "beat_dur": 0.5,
+                },
+                {
+                    "start_quartertime": 0.5,
+                    "midipitch": 67.0,
+                    "pitch_str": "G4",
+                    "accidental": None,
+                    "pitch_class": 7,
+                    "beat_dur": 0.5,
+                },
+                {
+                    "start_quartertime": 1.0,
+                    "midipitch": 75.0,
+                    "pitch_str": "Eb5",
+                    "accidental": music21.pitch.Accidental("-"),
+                    "pitch_class": 3,
+                    "beat_dur": 0.5,
+                },
+                {
+                    "start_quartertime": 1.5,
+                    "midipitch": 60.0,
+                    "pitch_str": "C4",
+                    "accidental": None,
+                    "pitch_class": 0,
+                    "beat_dur": 0.5,
+                },
+                {
+                    "start_quartertime": 1.5,
+                    "midipitch": 67.0,
+                    "pitch_str": "G4",
+                    "accidental": None,
+                    "pitch_class": 7,
+                    "beat_dur": 0.5,
+                },
+                {
+                    "start_quartertime": 1.5,
+                    "midipitch": 75.0,
+                    "pitch_str": "Eb5",
+                    "accidental": music21.pitch.Accidental("-"),
+                    "pitch_class": 3,
+                    "beat_dur": 0.5,
+                },
+            ],
+        }
+    )
+
+    return examples
+
+
+def test_abc_to_events(music21_test_examples):
+    test_results = {}
+    for example in music21_test_examples:
+        out = abc_to_events(example["in"])
+        msg = f"abc_to_events('{example['in']}') = {out}\nexpected: {example['out']}"
+        test_results[msg] = out == example["out"]
+    assert all(test_results.values()), (
+        "abc_to_events produced the incorrect output for: "
+        f"{[key for key, val in test_results.items() if val is False]}"
+    )
