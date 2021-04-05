685e971bc26812f2e14502b9eabdfe0cd028d09b
JamesOwers <james.f.owers+github@gmail.com>
2021-04-05 11:07:30 +0100

    Extract accidental name

--- double_jig_gen/tokenizers.py
+++ double_jig_gen/tokenizers.py
@@ -132,30 +132,39 @@ def merge_continuation_lines(lines: Sequence[str]) -> Sequence[str]:
 
 
 def abc_to_events(abc_str):
+    """Converts abc to a list of note events. Times are in number of quavers (quarters)."""
     abc = music21.converter.parse(abc_str, format="abc")
     # N.B. calling .flat on abc makes offset times stored in not.offset relative to the start of the piece, rather than relative to the containing stream.
     note_stream = abc.flat.getElementsByClass(["Note", "Chord"])
     events = []
     for element in note_stream:
         if isinstance(element, music21.note.Note):
+            accidental_str = (
+                element.pitch.accidental.fullName
+                if element.pitch.accidental is not None
+                else None
+            )
             events.append(
                 dict(
-                    start_quartertime=element.offset,
+                    start=element.offset,
                     midipitch=element.pitch.ps,
                     pitch_str=f"{element.name.replace('-', 'b')}{element.octave}",
-                    accidental=element.pitch.accidental,
+                    accidental=accidental_str,
                     pitch_class=element.pitch.pitchClass,
                     beat_dur=element.duration.quarterLength,
                 )
             )
         if isinstance(element, music21.chord.Chord):
             for pitch in element.pitches:
+                accidental_str = (
+                    pitch.accidental.fullName if pitch.accidental is not None else None
+                )
                 events.append(
                     dict(
-                        start_quartertime=element.offset,
+                        start=element.offset,
                         midipitch=pitch.ps,
                         pitch_str=f"{pitch.name.replace('-', 'b')}{pitch.octave}",
-                        accidental=pitch.accidental,
+                        accidental=accidental_str,
                         pitch_class=pitch.pitchClass,
                         beat_dur=element.duration.quarterLength,
                     )
@@ -163,73 +172,74 @@ def abc_to_events(abc_str):
     return events
 
 
-def parse_abc(abc_str: str) -> Mapping[str, str]:
-    """Get the required information from each tune.
+# # TODO: keeping since could be useful to parse metadata later
+# def parse_abc(abc_str: str) -> Mapping[str, str]:
+#     """Get the required information from each tune.
 
-    Extracts the metadata as defined in [1] from the string, plus the transcription
-    which is assumed not to have a prefix. Assumes all metadata is stated before the
-    transcription is started. Then adds everything remaining to the transcription.
+#     Extracts the metadata as defined in [1] from the string, plus the transcription
+#     which is assumed not to have a prefix. Assumes all metadata is stated before the
+#     transcription is started. Then adds everything remaining to the transcription.
 
-    Args:
-        abc_str: the string containing an abc file to parse.
+#     Args:
+#         abc_str: the string containing an abc file to parse.
 
-    Returns:
-        out_dict: a dictionary containing all the extracted values.
+#     Returns:
+#         out_dict: a dictionary containing all the extracted values.
 
-    See also:
-    [1] http://abcnotation.com/wiki/abc:standard:v2.1
-    """
-    lines = [line.strip() for line in abc_str.split("\n")]
+#     See also:
+#     [1] http://abcnotation.com/wiki/abc:standard:v2.1
+#     """
+#     lines = [line.strip() for line in abc_str.split("\n")]
 
-    # if a line ends with \ then merge with subsequent line
-    lines = merge_continuation_lines(lines)
+#     # if a line ends with \ then merge with subsequent line
+#     lines = merge_continuation_lines(lines)
 
-    valid_field = "|".join(ABC_FIELDS)
-    metadata_line_regex = re.compile(f"^({valid_field}):")
+#     valid_field = "|".join(ABC_FIELDS)
+#     metadata_line_regex = re.compile(f"^({valid_field}):")
 
-    _, metadata_end_idx = min(
-        (val, idx)
-        for (idx, val) in enumerate(
-            [bool(metadata_line_regex.match(line)) for line in lines]
-        )
-    )
+#     _, metadata_end_idx = min(
+#         (val, idx)
+#         for (idx, val) in enumerate(
+#             [bool(metadata_line_regex.match(line)) for line in lines]
+#         )
+#     )
 
-    out_dict = {}
-    for line in lines[:metadata_end_idx]:
-        meta_key, value = line.split(":", 1)
-        field_name = ABC_FIELDS[meta_key]
-        if field_name in out_dict:
-            concat_fields = ("tune title", "words")
-            if field_name not in concat_fields:
-                msg = (
-                    f"Tried to add {(field_name, value)} to {out_dict}."
-                    f"\nLines: {lines}"
-                )
-                raise ValueError(msg)
-            else:
-                out_dict[field_name] = f"{out_dict[field_name]} --- {line}"
-        out_dict[field_name] = value
-    if "meter" in out_dict:
-        tune_lines = [f"M:{out_dict['meter']}"]
-    else:
-        raise ValueError("An abc tune must have a meter")
-    if "key" in out_dict:
-        tune_lines += [f"K:{out_dict['key']}"]
-    else:
-        raise ValueError("An abc tune must have a key")
-    for line in lines[metadata_end_idx:]:
-        # If the line starts with a W: we assume the whole thing is words and move on
-        if line.upper().startswith("W:"):
-            if "words" not in out_dict:
-                out_dict["words"] = line
-            else:
-                out_dict["words"] = f"{out_dict['words']} --- {line}"
-            continue
-        # TODO: may require manual handling of things here e.g. mid line key changes
-        tune_lines.append(line)
-    out_dict["tune_lines"] = tune_lines
-    out_dict["tune_str"] = "\n".join(out_dict["tune_lines"])
-    return out_dict
+#     out_dict = {}
+#     for line in lines[:metadata_end_idx]:
+#         meta_key, value = line.split(":", 1)
+#         field_name = ABC_FIELDS[meta_key]
+#         if field_name in out_dict:
+#             concat_fields = ("tune title", "words")
+#             if field_name not in concat_fields:
+#                 msg = (
+#                     f"Tried to add {(field_name, value)} to {out_dict}."
+#                     f"\nLines: {lines}"
+#                 )
+#                 raise ValueError(msg)
+#             else:
+#                 out_dict[field_name] = f"{out_dict[field_name]} --- {line}"
+#         out_dict[field_name] = value
+#     if "meter" in out_dict:
+#         tune_lines = [f"M:{out_dict['meter']}"]
+#     else:
+#         raise ValueError("An abc tune must have a meter")
+#     if "key" in out_dict:
+#         tune_lines += [f"K:{out_dict['key']}"]
+#     else:
+#         raise ValueError("An abc tune must have a key")
+#     for line in lines[metadata_end_idx:]:
+#         # If the line starts with a W: we assume the whole thing is words and move on
+#         if line.upper().startswith("W:"):
+#             if "words" not in out_dict:
+#                 out_dict["words"] = line
+#             else:
+#                 out_dict["words"] = f"{out_dict['words']} --- {line}"
+#             continue
+#         # TODO: may require manual handling of things here e.g. mid line key changes
+#         tune_lines.append(line)
+#     out_dict["tune_lines"] = tune_lines
+#     out_dict["tune_str"] = "\n".join(out_dict["tune_lines"])
+#     return out_dict
 
 
 class ABCParser:
@@ -237,14 +247,12 @@ class ABCParser:
 
     def __init__(self, abc_str):
         self.abc_str = abc_str
-        self.abc_dict = parse_abc(abc_str)
-        self.tune_str = self.abc_dict["tune_str"]
         self.abc_handler = music21.abcFormat.ABCHandler()
-        self._tokens = None
-
-    @property
-    def tokens(self):
-        if self._tokens is None:
-            self.abc_handler.tokenize(self.tune_str)
-            self._tokens = [token.src for token in self.abc_handler.tokens]
-        return self._tokens
+        self.abc_handler.tokenize(self.abc_str)
+        self._tokens_music21 = self.abc_handler.tokens
+        self.metadata = [
+            token.src
+            for token in self._tokens_music21
+            if isinstance(token, music21.abcFormat.ABCMetadata)
+        ]
+        self.events = abc_to_events(abc_str)
